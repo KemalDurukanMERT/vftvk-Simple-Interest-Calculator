86a063446e9bbd958c6ed301386f9faeb5750ccf
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-11-08 02:15:37 +0100

    last version

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const rangeInput = document.querySelector(".rangeInput");
+const btn = document.querySelector(".btn");
 const year = document.querySelector("#year");
 console.log(rangeInput);
 
@@ -9,13 +10,25 @@ rangeInput.addEventListener("input", (e) => {
   rangeArea.innerHTML = `${e.target.value} %`;
 });
 
-function compute() {
-  p = document.getElementById("principal").value;
-  interest = document.querySelector(".interest");
-  interest.innerHTML = `
-  <p>If you deposit <mark>${p}</mark></p>
-  <p>at an interest rate of <mark>${rangeInput.value}%</mark>.</p>
-  <p>You will receive an amount of <mark>${p*(rangeInput.value/100)*(year.value)}</mark>.</p>
-  <p>in the year <mark>${(new Date()).getFullYear()+year.value}</mark></p>
-  `
-}
+btn.addEventListener("click", (e)=>{
+    p = document.getElementById("principal").value;
+    if (p <= 0) {
+        e.preventDefault()
+        alert("Enter a positive number.");
+        document.getElementById("principal").focus();
+    } else {
+        interest = document.querySelector(".interest");
+        interest.innerHTML = `
+        <p>If you deposit <mark>${p}</mark></p>
+        <p>at an interest rate of <mark>${rangeInput.value}%</mark>.</p>
+        <p>You will receive an amount of <mark>${
+            p * (rangeInput.value / 100) * year.value
+        }</mark>.</p>
+        <p>in the year <mark>${
+            new Date().getFullYear() + Number(year.value)
+        }</mark></p>
+        `;
+    }
+})
+    
+    
